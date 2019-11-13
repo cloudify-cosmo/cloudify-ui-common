3d8e53eed0ed906fe652ad675feb8df724d6aaa5
Jakub Niezgoda <jakub.niezgoda@nitarch.pl>
2019-11-13 12:20:21 +0100

    CY-1487 - Export script for publishing version to NPM (#19)
    * Extracted common version publishing part of publish-version.sh script into create-version.sh 
    * Added new script to exported scripts

--- ci/publish-version.sh
+++ ci/publish-version.sh
@@ -1,44 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-log() {
-    LOG_PREFIX="publish-version.sh:"
-    echo -e $LOG_PREFIX $@
-}
+. scripts/create-version.sh
 
-MAIN_BRANCH=${MAIN_BRANCH:-master}
-log "Switching to main branch (${MAIN_BRANCH}) and checking if its up-to-date...";
-git checkout ${MAIN_BRANCH}
-git fetch
-git diff origin/${MAIN_BRANCH} --exit-code --compact-summary
-
-log "Running tests and build..."
-npm run test:build
-
-NEXT_VERSION=$(npm version --no-git-tag-version "$@")
-log "Creating cloudify-ui-common ${NEXT_VERSION}...";
-
-log "Creating version publish branch...";
-NEXT_VERSION_BRANCH="publish-${NEXT_VERSION}"
-git checkout -b ${NEXT_VERSION_BRANCH}
-git add package.json package-lock.json
-git commit -m "Bump version to ${NEXT_VERSION}"
-git tag ${NEXT_VERSION}
-
-log "This is the last step before CircleCI-based npm publish.";
-log "Are you sure you want to trigger cloudify-ui-common ${NEXT_VERSION} version publish?";
-select choice in "Yes" "No"; do
-case "$choice" in
-    Yes)
-        log "Pushing changes...";
-        git push origin ${NEXT_VERSION_BRANCH} tag ${NEXT_VERSION}
-        git checkout ${MAIN_BRANCH}
-        break;;
-    *)
-        log "Reverting changes...";
-        git checkout ${MAIN_BRANCH}
-        git tag -d ${NEXT_VERSION}
-        git branch -D ${NEXT_VERSION_BRANCH}
-        break;;
-esac
-done
+createVersion "$@"
--- scripts/README.md
+++ scripts/README.md
@@ -1,5 +1,9 @@
 ## Scripts
 
+### create-version.sh
+
+Provides helper function for creating new version of NPM package by creating a new branch with version change commit and pushing that branch to the remote repository.
+
 ### upload-package.sh
 
 Provides helper function for uploading package file to the remote machine using SSH and running specified command remotely.
