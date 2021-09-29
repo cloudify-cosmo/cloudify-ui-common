9a14a3a25b605cb7ce5f1618b3423510e2743afa
kubama <jakub.madej@amartus.com>
2021-09-29 17:24:25 +0200

    RD-2993 Enabled component testing in Cypress setup and configuration (#77)

--- cypress/src/plugins/index.ts
+++ cypress/src/plugins/index.ts
@@ -8,6 +8,8 @@
 // https://on.cypress.io/plugins-guide
 // ***********************************************************
 import coverageTask from '@cypress/code-coverage/task';
+import { startDevServer } from '@cypress/webpack-dev-server';
+import type { StartDevServer } from '@cypress/webpack-dev-server';
 
 /**
  * Performs common plugin and configuration setup - registers coverage plugin and sets `baseUrl` property if MANAGER_IP
@@ -17,9 +19,14 @@ import coverageTask from '@cypress/code-coverage/task';
  *           @see https://nodejs.org/api/events.html#events_emitter_on_eventname_listener
  *           @see https://docs.cypress.io/api/events/catalog-of-events.html#App-Events
  * @param config Cypress configuration object to be updated
+ * @param webpackConfig Webpack configuration
  * @returns Updated configuration
  */
-const setupPluginsAndConfig: Cypress.PluginConfig = (on, config) => {
+const setupPluginsAndConfig = (
+    on: Cypress.PluginEvents,
+    config: Cypress.PluginConfigOptions,
+    webpackConfig: StartDevServer['webpackConfig']
+): Cypress.PluginConfigOptions => {
     coverageTask(on, config);
 
     const { MANAGER_IP, MANAGER_PROTOCOL = 'http' } = process.env;
@@ -28,6 +35,15 @@ const setupPluginsAndConfig: Cypress.PluginConfig = (on, config) => {
     }
 
     console.info(`Testing on: ${config.baseUrl}`);
+
+    if (config.testingType === 'component') {
+        on('dev-server:start', options =>
+            startDevServer({
+                options,
+                webpackConfig
+            })
+        );
+    }
 
     return config;
 };
