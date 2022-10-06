9135c83cd4a5cce20749197a5eeced0647a6b113
Jakub Niezgoda <jakub.niezgoda@amartus.com>
2022-10-06 08:53:48 +0200

    Update mechanism to allow sub-packages publishing

--- packages/scripts/create-version.sh
+++ packages/scripts/create-version.sh
@@ -3,7 +3,7 @@ set -e
 
 #===================================================================================
 # Usage:
-#   createVersion <major|minor|patch>
+#   create-version.sh <version-type> [<package-name>]
 #
 # Description:
 #   Create new version of package to be published on NPM.
@@ -13,74 +13,106 @@ set -e
 #   MAIN_BRANCH - (optional) Repository main branch (default: master)
 #
 # Arguments:
-#   $1 (version type) - "patch", "minor" and "major" strings allowed
+#   $1 (version type) - `npm version` strings allowed
+#                       see https://docs.npmjs.com/cli/v8/commands/npm-version#workspaces
+#   $2 (package name) - last portion of the package name inside `packages` directory
 #
 # Returns:
 #   None
 #===================================================================================
 
-createVersion() {
-    log() {
-        LOG_PREFIX="create-version.sh:"
-        echo -e $LOG_PREFIX $@
-    }
-
-    NPM_PACKAGE_NAME=`cut -d "=" -f 2 <<< $(npm run env | grep "npm_package_name")`
-
-    MAIN_BRANCH=${MAIN_BRANCH:-master}
-    log "Switching to main branch (${MAIN_BRANCH}) and checking if its up-to-date...";
-    git checkout ${MAIN_BRANCH} || {
-      log "Error during checking out ${MAIN_BRANCH} branch"
-      false
-    }
-    git fetch || {
-      log "Error during fetching data from repository"
-      false
-    }
-    git pull || {
-      log "Error during pulling ${MAIN_BRANCH} branch"
-      false
-    }
-    git diff origin/${MAIN_BRANCH} --exit-code --compact-summary || {
-      log "Working directory not clean. Delete or push your changes to ${MAIN_BRANCH}}."
-      false
-    }
-
-    log "Running tests and build..."
-    npm run test || {
-      log "Running tests failed"
-      false
-    }
-    npm run build || {
-      log "Running build failed"
-      false
-    }
-
-    NEXT_VERSION=$(npm version --no-git-tag-version "$@")
-    log "Creating ${NPM_PACKAGE_NAME} ${NEXT_VERSION}...";
-
-    log "Creating version publish branch...";
-    NEXT_VERSION_BRANCH="publish-${NEXT_VERSION}"
-    git checkout -b ${NEXT_VERSION_BRANCH}
-    git add package.json package-lock.json
-    git commit -m "Bump version to ${NEXT_VERSION}"
-    git tag ${NEXT_VERSION}
-
-    log "This is the last step before CI-based npm publish.";
-    log "Are you sure you want to trigger ${NPM_PACKAGE_NAME} ${NEXT_VERSION} version publish?";
-    select choice in "Yes" "No"; do
-    case "$choice" in
-        Yes)
-            log "Pushing changes...";
-            git push origin ${NEXT_VERSION_BRANCH} tag ${NEXT_VERSION}
-            git checkout ${MAIN_BRANCH}
-            break;;
-        *)
-            log "Reverting changes...";
-            git checkout ${MAIN_BRANCH}
-            git tag -d ${NEXT_VERSION}
-            git branch -D ${NEXT_VERSION_BRANCH}
-            break;;
-    esac
-    done
+log() {
+    LOG_PREFIX="create-version.sh:"
+    echo -e $LOG_PREFIX $@
 }
+
+VERSION_TYPE=$1
+PACKAGE_NAME=$2
+
+if [ -n "$PACKAGE_NAME" ]; then
+    IS_WORKSPACE_PACKAGE=1
+    cd packages/$2 || {
+        log "Invalid package name provided"
+        false
+    }
+fi
+NPM_PACKAGE_NAME=`cut -d "=" -f 2 <<< $(npm run env | grep "npm_package_name")`
+
+MAIN_BRANCH=${MAIN_BRANCH:-master}
+log "Switching to main branch (${MAIN_BRANCH}) and checking if its up-to-date...";
+git checkout ${MAIN_BRANCH} || {
+  log "Error during checking out ${MAIN_BRANCH} branch"
+  false
+}
+git fetch || {
+  log "Error during fetching data from repository"
+  false
+}
+git pull || {
+  log "Error during pulling ${MAIN_BRANCH} branch"
+  false
+}
+git diff origin/${MAIN_BRANCH} --exit-code --compact-summary || {
+  log "Working directory not clean. Delete or push your changes to ${MAIN_BRANCH}}."
+  false
+}
+
+log "Running tests and build..."
+npm run lint --if-present || {
+  log "Running lint check failed"
+  false
+}
+npm run check-types --if-present || {
+  log "Running types check failed"
+  false
+}
+npm run test --if-present || {
+  log "Running tests failed"
+  false
+}
+npm run build --if-present || {
+  log "Running build failed"
+  false
+}
+
+NPM_VERSION_OPTIONS="--no-git-tag-version $VERSION_TYPE"
+if [[ $VERSION_TYPE == pre* ]]; then
+    NPM_VERSION_OPTIONS="$NPM_VERSION_OPTIONS --preid pre"
+fi
+echo NPM_VERSION_OPTIONS = $NPM_VERSION_OPTIONS
+npm version $NPM_VERSION_OPTIONS
+NEXT_VERSION=v$(node -p "require('./package.json').version")
+log "Creating ${NPM_PACKAGE_NAME} ${NEXT_VERSION}...";
+
+log "Creating version publish branch...";
+if [ -n "$IS_WORKSPACE_PACKAGE" ]; then
+    NEXT_VERSION_BRANCH="publish_${PACKAGE_NAME}_${NEXT_VERSION}"
+else
+    NEXT_VERSION_BRANCH="publish_${NEXT_VERSION}"
+fi
+
+git checkout -b ${NEXT_VERSION_BRANCH}
+git add package.json
+if [ -z "$IS_WORKSPACE_PACKAGE" ]; then
+    git add package-lock.json
+fi
+git commit -m "Bump version to ${NEXT_VERSION}"
+git tag ${NEXT_VERSION}
+
+log "This is the last step before CI-based npm publish.";
+log "Are you sure you want to trigger publish ${NPM_PACKAGE_NAME} ${NEXT_VERSION}?";
+select choice in "Yes" "No"; do
+case "$choice" in
+    Yes)
+        log "Pushing changes...";
+        git push origin ${NEXT_VERSION_BRANCH} tag ${NEXT_VERSION}
+        git checkout ${MAIN_BRANCH}
+        break;;
+    *)
+        log "Reverting changes...";
+        git checkout ${MAIN_BRANCH}
+        git tag -d ${NEXT_VERSION}
+        git branch -D ${NEXT_VERSION_BRANCH}
+        break;;
+esac
+done
