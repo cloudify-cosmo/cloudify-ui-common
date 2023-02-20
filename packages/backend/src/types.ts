e939ade82cd3e4de9d634a18722c382b2a2ffc93
Jakub Madej <jakub.madej@cloudify.co>
2023-02-20 15:51:51 +0100

    Use single eslint disable

--- packages/backend/src/types.ts
+++ packages/backend/src/types.ts
@@ -1,3 +1,4 @@
+/* eslint-disable camelcase */
 export interface BlueprintElement {
     type: string;
     properties?: Record<string, any>;
@@ -5,9 +6,7 @@ export interface BlueprintElement {
 
 export interface Relationship extends BlueprintElement {
     target: string;
-    // eslint-disable-next-line camelcase
     source_interfaces?: Interfaces;
-    // eslint-disable-next-line camelcase
     target_interfaces?: Interfaces;
 }
 
@@ -43,7 +42,6 @@ export interface Input {
     default?: any;
     required?: boolean;
     hidden?: boolean;
-    // eslint-disable-next-line camelcase
     display_label?: string;
     display?: Display;
 }
@@ -58,22 +56,17 @@ export interface Label {
 }
 
 export interface Blueprint {
-    // eslint-disable-next-line camelcase
     tosca_definitions_version?: string;
     description?: string;
     imports: string[] | null;
     plugins?: Record<string, string>;
-    // eslint-disable-next-line camelcase
     node_types?: Record<string, NodeType>;
-    // eslint-disable-next-line camelcase
     relationships?: Record<string, { derived_from: string }>;
     inputs?: Record<string, Input>;
     labels?: Record<string, Label>;
-    // eslint-disable-next-line camelcase
     blueprint_labels?: Record<string, Label>;
     outputs?: Record<string, Output>;
     capabilities?: Record<string, Output>;
-    // eslint-disable-next-line camelcase
     node_templates?: Record<string, NodeTemplate>;
     groups?: Groups;
 }
